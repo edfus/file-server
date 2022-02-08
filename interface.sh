95ad24bbd3be7151a78fc76897a2f58ecc446c58
edfus <edfusedfus@gmail.com>
2022-02-09 06:34:11 +0800

    feat: npm tag interface

---
+++ interface.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+intermediateignore=".ignored-1-$RANDOM"
+intermediatepackagejson=".ignored-2-$RANDOM"
+cp .npmignore "$intermediateignore"
+cp package.json "$intermediatepackagejson"
+
+cat>>.npmignore<<'eof'
+
+bin
+img
+lib
+eof
+
+node -e 'const p = require("./package.json"); delete p.bin; fs.writeFileSync("package.json", JSON.stringify(p, null, 2), "utf-8")'
+npm publish --tag interface
+
+mv "$intermediateignore" .npmignore
+mv "$intermediatepackagejson" package.json
